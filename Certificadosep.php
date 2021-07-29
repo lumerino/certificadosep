203475e67c90a1c85e19aad5c30eb4567f4f9ff0
lumerino <68938246+lumerino@users.noreply.github.com>
2021-07-28 22:08:11 -0500

    Update Certificadosep.php

--- Certificadosep.php
+++ Certificadosep.php
@@ -1,8 +1,4 @@
 <?php
-/*Clase para crear XML de certificado electrónico de la SEP MEC V2.0
-Creada por Ing. MTW Luis Gerardo Merino Figueroa
-05/03/2020
-*/
 class certificadosep
 {
 	private $xml;
@@ -38,30 +34,32 @@ $this->xml_ipes($arr);
 $this->xml_Rvoe($arr);
 $this->xml_carrera($arr);
 $this->xml_alumno($arr);
-$this->xml_asignaturas($arr);
 $this->xml_expedicion($arr);
+$this->xml_asignaturas($arr);
+
 }
-private function xml_generales($arr)
+function xml_generales($arr)
 {
 $xml=$this->getXml();
 $root=$this->getRoot();
 $root =$xml->createElement("Dec");
 $root=$xml->appendChild($root);
-$this->cargaAtt($root, array("xmlns"=>"https://www.siged.sep.gob.mx/certificados",
-
-
+$this->cargaAtt($root, array(
 						"version"=>$arr["version"],
 						"tipoCertificado"=>$arr["tipoCertificado"],
 						"folioControl"=>$arr["folioControl"],
-						"Sello"=>$arr["Sello"],
-						"certificadoResponsable"=>$arr["certificadoResponsable"],
+					//	"sello"=>$arr["Sello"],
+						"sello"=>"@",
+					//	"certificadoResponsable"=>$arr["certificadoResponsable"],
+						"certificadoResponsable"=>"@",
 						"noCertificadoResponsable"=>$arr["noCertificadoResponsable"],
+						"xmlns"=>"https://www.siged.sep.gob.mx/certificados/",
 					 )
 				 );
 $this->setXml($xml);
 $this->setRoot($root);
 }
-private function xml_Ipes($arr)
+function xml_Ipes($arr)
 {
 	$xml=$this->getXml();
 	$root=$this->getRoot();
@@ -70,11 +68,11 @@ private function xml_Ipes($arr)
 
 	$responsable=$ipes->appendChild($responsable);
 	$this->cargaAtt($ipes,array("idNombreInstitucion"=>$arr['Ipes']["idNombreInstitucion"],
-							"nombreInstitucion"=>$arr['Ipes']["nombreInstitucion"],
+							//"nombreInstitucion"=>$arr['Ipes']["nombreInstitucion"],
 							"idCampus"=>$arr['Ipes']["idCampus"],
-							"campus"=>$arr['Ipes']["campus"],
+							//"campus"=>$arr['Ipes']["campus"],
 							"idEntidadFederativa"=>$arr['Ipes']["idEntidadFederativa"],
-							"entidadFederativa"=>$arr['Ipes']["entidadFederativa"],
+							//"entidadFederativa"=>$arr['Ipes']["entidadFederativa"],
 						 )
 					 );
 					 $this->cargaAtt($responsable,array("curp"=>$arr['Ipes']["Responsable"]["curp"],
@@ -82,7 +80,7 @@ private function xml_Ipes($arr)
 											"primerApellido"=>$this->fix_chr($arr['Ipes']["Responsable"]["primerApellido"]),
 											"segundoApellido"=>$this->fix_chr($arr['Ipes']["Responsable"]["segundoApellido"]),
 											"idCargo"=>$arr['Ipes']["Responsable"]["idCargo"],
-											"cargo"=>$this->fix_chr($arr['Ipes']["Responsable"]["cargo"]),
+											//"cargo"=>$this->fix_chr($arr['Ipes']["Responsable"]["cargo"]),
 
 			 							 )
 			 						 );
@@ -91,7 +89,7 @@ private function xml_Ipes($arr)
 	$this->setRoot($root);
 
 }
-private function xml_Rvoe($arr)
+function xml_Rvoe($arr)
 {
 	$xml=$this->getXml();
 	$root=$this->getRoot();
@@ -105,15 +103,15 @@ private function xml_Rvoe($arr)
 	$this->setRoot($root);
 
 }
-private function xml_carrera($arr)
+function xml_carrera($arr)
 {
 	$xml=$this->getXml();
 	$root=$this->getRoot();
 	$carrera =$xml->createElement("Carrera");
 	$carrera=$root->appendChild($carrera);
 	$this->cargaAtt($carrera,array("idCarrera"=>$arr['Carrera']["idCarrera"],
-							"claveCarrera"=>$arr['Carrera']["claveCarrera"],
-							"nombreCarrera"=>$this->fix_chr($arr['Carrera']["nombreCarrera"]),
+							//"claveCarrera"=>$arr['Carrera']["claveCarrera"],
+							//"nombreCarrera"=>$this->fix_chr($arr['Carrera']["nombreCarrera"]),
 							"idTipoPeriodo"=>$arr['Carrera']["idTipoPeriodo"],
 							"clavePlan"=>$arr['Carrera']["clavePlan"],
 							"idNivelEstudios"=>$arr['Carrera']["idNivelEstudios"],
@@ -125,7 +123,7 @@ private function xml_carrera($arr)
 	$this->setXml($xml);
 	$this->setRoot($root);
 }
-private function xml_alumno($arr)
+function xml_alumno($arr)
 {
 	$xml=$this->getXml();
 	$root=$this->getRoot();
@@ -138,29 +136,29 @@ private function xml_alumno($arr)
 		"segundoApellido"=>	$arr['Alumno']["segundoApellido"],
 		"idGenero"=>	$arr['Alumno']["idGenero"],
 		"fechaNacimiento"=>	$arr['Alumno']["fechaNacimiento"],
-		"foto"=>	$arr['Alumno']["foto"],
+		//"foto"=>	"",
 	)
 					 );
 	$this->setXml($xml);
 	$this->setRoot($root);
 }
-private function xml_expedicion($arr)
+function xml_expedicion($arr)
 {
 	$xml=$this->getXml();
 	$root=$this->getRoot();
 	$expedicion =$xml->createElement("Expedicion");
 	$expedicion=$root->appendChild($expedicion);
 	$this->cargaAtt($expedicion,array("idTipoCertificacion"=>	$arr['Expedicion']["idTipoCertificacion"],
-	"tipoCertificaciontipoCertificacion"=>	$arr['Expedicion']["tipoCertificacion"],
+	//"tipoCertificaciontipoCertificacion"=>	$arr['Expedicion']["tipoCertificacion"],
 		"fecha"=>	$arr['Expedicion']["fecha"],
 		"idLugarExpedicion"=>	$arr['Expedicion']["idLugarExpedicion"],
-		"lugarExpedicion"=>	$arr['Expedicion']["lugarExpedicion"],
+		//"lugarExpedicion"=>	$arr['Expedicion']["lugarExpedicion"],
 	)
 					 );
 	$this->setXml($xml);
 	$this->setRoot($root);
 }
-private function xml_asignaturas($arr)
+function xml_asignaturas($arr)
 {
 	$xml=$this->getXml();
 	$root=$this->getRoot();
@@ -170,7 +168,7 @@ $asignaturas=$root->appendChild($asignaturas);
 							"asignadas"=>	$arr['Asignaturas']["asignadas"],
 							"promedio"=>	$arr['Asignaturas']["promedio"],
 							"totalCreditos"=>	$arr['Asignaturas']["totalCreditos"],
-							"CreditosObtenidos"=>	$arr['Asignaturas']["CreditosObtenidos"],
+							"creditosObtenidos"=>	$arr['Asignaturas']["CreditosObtenidos"],
 						 )
 					 );
 	for($i=0;$i<sizeof($arr['Asignaturas']["Asignatura"]);$i++)
@@ -178,14 +176,14 @@ $asignaturas=$root->appendChild($asignaturas);
 		$asignatura=$xml->createElement("Asignatura");
 		$asignatura=$asignaturas->appendChild($asignatura);
 		$this->cargaAtt($asignatura,array("idAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["idAsignatura"],
-								"claveAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["claveAsignatura"],
-								"nombre"=>$this->fix_chr($arr['Asignaturas']["Asignatura"][$i]["nombre"]),
+								//"claveAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["claveAsignatura"],
+								//"nombre"=>$this->fix_chr($arr['Asignaturas']["Asignatura"][$i]["nombre"]),
 								"ciclo"=>$arr['Asignaturas']["Asignatura"][$i]["ciclo"],
 								"calificacion"=>$arr['Asignaturas']["Asignatura"][$i]["calificacion"],
-								"idTipoAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["idTipoAsignatura"],
 								"idObservaciones"=>$arr['Asignaturas']["Asignatura"][$i]["idObservaciones"],
-								"observaciones"=>$arr['Asignaturas']["Asignatura"][$i]["observaciones"],
-								"tipoAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["tipoAsignatura"],
+								"idTipoAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["idTipoAsignatura"],
+								//"observaciones"=>$arr['Asignaturas']["Asignatura"][$i]["observaciones"],
+								//"tipoAsignatura"=>$arr['Asignaturas']["Asignatura"][$i]["tipoAsignatura"],
 								"creditos"=>$arr['Asignaturas']["Asignatura"][$i]["creditos"],
 							)
 						 );
@@ -196,7 +194,7 @@ $asignaturas=$root->appendChild($asignaturas);
 	$this->setRoot($root);
 
 }
-private function cargaAtt(&$nodo, $attr) {
+public function cargaAtt(&$nodo, $attr) {
 
 	foreach ($attr as $key => $val) {
 		for ($i=0;$i<strlen($val); $i++) {
@@ -214,27 +212,43 @@ private function cargaAtt(&$nodo, $attr) {
 		}
 	}
 	}
-	public function xml_sella($arr) {
+	function xml_sella($arr) {
 
 		$certificado = $arr['fiel'];
 		$llave=$arr['llave'];
 		$password=$arr['password'];
 		$cadena_original=$arr['cadena_original'];
+
 				// Ruta al archivo
 		// Obtiene la llave privada del Certificado de Sello Digital (CSD),
 		//    Ojo , Nunca es la FIEL/FEA
 		$certificadom=$this->getCertificate($_SERVER['DOCUMENT_ROOT']."/managecesba/cer/".$certificado, false );
+			$certificadofisico=$this->generateCertificate($_SERVER['DOCUMENT_ROOT']."/managecesba/cer/".$certificado);
 		$claveprivada=$this->getPrivateKey($_SERVER['DOCUMENT_ROOT']."/managecesba/cer/".$llave, $password);
 		$claveprivadafisica=$this->generatePrivateKey($_SERVER['DOCUMENT_ROOT']."/managecesba/cer/".$llave, $password);
 		/*
 		$this->root->setAttribute("Certificado",$certificadom);
 		$this->satxmlsv33_genera_cadena_original();*/
+
+		$this->root->setAttribute("certificadoResponsable",$certificadom);
+//var_dump($cadena_original);
+//die();
 		$sello=$this->signData($claveprivada,trim($cadena_original));
+
+		//echo $sello;
+$this->root->setAttribute("sello",$sello);
+// $public_key = openssl_pkey_get_public(file_get_contents($_SERVER['DOCUMENT_ROOT']."/managecesba/tempcer.cer.pem"));
+// $fp = fopen($_SERVER['DOCUMENT_ROOT']."/managecesba/tempcer.cer.pem", "r");
+// $cert = fread($fp, 8192);
+// fclose($fp);
+// $result = openssl_verify(trim($cadena_original), $Firma,$public_key, "sha256WithRSAEncryption");
+// echo "<br><br> Resultado ".$result;
+// die();
 
 
 return $sello;
 		}
-		public function gen_cadena_original($arr)
+		function gen_cadena_original($arr)
 		{
 			$cadena="|";
 			$cadena.="|".$arr['version'];
@@ -260,8 +274,8 @@ return $sello;
 			$cadena.="|".$arr['Alumno']["segundoApellido"];
 			$cadena.="|".$arr['Alumno']["idGenero"];
 			$cadena.="|".$arr['Alumno']["fechaNacimiento"];
-			$cadena.="|".$arr['Alumno']["foto"];
-				$cadena.="|".$arr['Alumno']["firmaAutografa"];
+			$cadena.="|";
+				$cadena.="|";
 			$cadena.="|".$arr['Expedicion']["idTipoCertificacion"];
 			$cadena.="|".$arr['Expedicion']["fecha"];
 			$cadena.="|".$arr['Expedicion']["idLugarExpedicion"];
@@ -278,23 +292,25 @@ return $sello;
 			$cadena.="|".$arr['Asignaturas']["Asignatura"][$i]["creditos"];
 
 	}
-			$cadena.="|";
+			$cadena.="||";
 
 			return trim($cadena);
 		}
-		private function signData ( $key, $data )
+		function signData ( $key, $data )
 		{
 		$pkeyid = openssl_get_privatekey( $key );
 
 		// On 2011 Signing algorythm changes from MD5 to SHA1 (Thanks to eDwaRd for the reminder)
-		if ( openssl_sign( $data, $cryptedata, $pkeyid, OPENSSL_ALGO_SHA256) ) {
+		if ( openssl_sign( utf8_encode($data), $cryptedata,  $pkeyid, OPENSSL_ALGO_SHA256) ) {
+		//if ( openssl_sign( $data, $cryptedata,  $pkeyid, "sha256WithRSAEncryption") ) {
 
-		openssl_free_key( $pkeyid );
+		openssl_free_key( 	$pkeyid );
 
 		return base64_encode( $cryptedata );
+	//		return $cryptedata;
 		}
 		}
-		public function getCertificate ( $cer_path, $to_string = true )
+		function getCertificate ( $cer_path, $to_string = true )
 		{
 		$cmd = 'openssl x509 -inform DER -outform PEM -in '.$cer_path.' -pubkey';
 		if ( $result = shell_exec( $cmd ) ) {
@@ -313,7 +329,7 @@ return $sello;
 
 		return false;
 		}
-		private function generateCertificate($cer_path){
+		function generateCertificate($cer_path){
 
 		$cmd = 'openssl x509 -inform DER -outform PEM -in '.$cer_path.' -out tempcer.cer.pem';
 		if ( $result = shell_exec( $cmd ) ) {
@@ -325,7 +341,7 @@ return $sello;
 		return false;
 
 		}
-		private function generatePrivateKey($key_path,$password){
+		function generatePrivateKey($key_path,$password){
 
 		$cmd = 'openssl pkcs8 -inform DER -in '.$key_path.' -out tempkey.key.pem -passin pass:'.$password;
 		if ( $result = shell_exec( $cmd ) ) {
@@ -337,7 +353,7 @@ return $sello;
 		return false;
 
 		}
-		private function getPFX ( $cer_path,$key_path,$password )
+		function getPFX ( $cer_path,$key_path,$password )
 		{
 		$cmd = 'openssl pkcs12 -export -in'.$cer_path.'-inkey '.$key_path.' -passout pass:'.$password;
 		if ( $result = shell_exec( $cmd ) ) {
@@ -348,7 +364,7 @@ return $sello;
 
 		return false;
 		}
-		private function getPrivateKey ( $key_path, $password )
+		function getPrivateKey ( $key_path, $password )
 		{
 		$cmd = 'openssl pkcs8 -inform DER -in '.$key_path.' -passin pass:'.$password;
 		if ( $result = shell_exec( $cmd ) ) {
@@ -359,7 +375,7 @@ return $sello;
 
 		return false;
 		}
-		private function fix_chr($nomb) {
+		function fix_chr($nomb) {
 			$nomb = str_replace(array(".","/")," ",$nomb);
 			return ($nomb);
 		}
